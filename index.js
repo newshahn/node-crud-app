55f6ffb148a0f41ddc4db4c63fc934f125e94505
Newsha Hosseiny Nik <nioosha.h@gmail.com>
2021-07-03 17:37:41 +0430

    added homepage routing

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ const PORT = 5000;
 
 app.use(bodyParser.json());
 
+app.get("/", (req, res) => {
+  console.log("[TEST]");
+
+  res.send("Hello From Homepage.");
+});
+
 app.listen(PORT, () =>
   console.log(`Server Running on port: http://localhost:${PORT}`)
 );
