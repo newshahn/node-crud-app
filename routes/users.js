32ab4fd9044b1c03d08c655d6fbff6dc6ebdef33
Newsha Hosseiny Nik <nioosha.h@gmail.com>
2021-07-04 14:54:49 +0430

    added a delete http request which delets a user by id

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ import { v4 as uuidv4 } from "uuid";
 
 const router = express.Router();
 
-const users = [];
+let users = [];
 
 // all routes in here are starting with /users
 router.get("/", (req, res) => {
@@ -22,9 +22,14 @@ router.post("/", (req, res) => {
 
 router.get("/:id", (req, res) => {
   const { id } = req.params;
-
   const foundUser = users.find((user) => user.id === id);
   res.send(foundUser);
+});
+
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  users = users.filter((user) => user.id !== id);
+  res.send(`user with the id of ${id} was deleted from the database`);
 });
 
 export default router;
