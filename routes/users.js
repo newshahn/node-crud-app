0033142c9f0a57fe18ecfc0ea608b68fb3ed6836
Newsha Hosseiny Nik <nioosha.h@gmail.com>
2021-07-04 14:27:06 +0430

    added logic for finding a user by its id with a get reqiest

--- routes/users.js
+++ routes/users.js
@@ -20,4 +20,11 @@ router.post("/", (req, res) => {
   );
 });
 
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+
+  const foundUser = users.find((user) => user.id === id);
+  res.send(foundUser);
+});
+
 export default router;
