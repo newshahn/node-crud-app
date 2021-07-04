e6c5ddfd661775803345341de9df3ca8a32df52e
Newsha Hosseiny Nik <nioosha.h@gmail.com>
2021-07-04 14:08:08 +0430

    added a users array as a mock database. fixed the post request for adding a new user

--- routes/users.js
+++ routes/users.js
@@ -2,10 +2,31 @@ import express from "express";
 
 const router = express.Router();
 
-// all routes in here are starting with /users
+const users = [
+  {
+    firstName: "John",
+    lastName: "Doe",
+    age: 25,
+  },
+  {
+    firstName: "Jane",
+    lastName: "Doe",
+    age: 26,
+  },
+];
 
+// all routes in here are starting with /users
 router.get("/", (req, res) => {
-  res.send("Hello");
+  res.send(users);
+});
+
+router.post("/", (req, res) => {
+  const user = req.body;
+  users.push(user);
+
+  res.send(
+    `user with name of ${user.firstName} and last name of ${user.lastName} was added to the database`
+  );
 });
 
 export default router;
